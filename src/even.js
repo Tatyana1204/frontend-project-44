67b15f818374911c451d2683094da18063c62f71
Tatyana1204 <Smetanina2002sme@gmail.com>
2024-02-22 10:42:39 +0300

    revision even.js

--- src/even.js
+++ src/even.js
@@ -4,27 +4,27 @@ console.log("Welcome to the Brain Games!");
 let user = getUserName();
 const isEven = () => {
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
-let result = '';
+let correctAnswer = '';
 let answer = '';
-let iter = 0;
-let correct = 'Correct!';
-while((correct === 'Correct!') && (iter < 3)){
-    let number = Math.floor(Math.random() * 101); 
+let iter = 3;
+let isCorrect = 'Correct!';
+while((isCorrect === 'Correct!') && (iter > 0)){
+    const number = Math.floor(Math.random() * 101); 
     console.log('Question: '+ number);
     answer = readlineSync.question('Your answer: ');
-    result = (number %2 === 0)? 'yes':'no';
-    if(answer === result){
-        console.log(correct);
-            iter++;
+    correctAnswer = (number %2 === 0)? 'yes':'no';
+    if(answer === correctAnswer){
+        console.log(isCorrect);
+            iter--;
         }
     else {
-        correct = false;
-        console.log(' "'+ answer + '" '+ 'is wrong answer ;(. Correct answer was' + ' "'  + result + '". ' + "Let's try again, " + user);
+        isCorrect = false;
+        const sadMessage = `"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}". Let's try again, ${user})`;
+        console.log(sadMessage);
+        return false;
         }
 }
-if(iter === 3){
-    console.log("Congratulations, " + user);
-}
+console.log("Congratulations, " + user);
 };
 
 export default isEven;
