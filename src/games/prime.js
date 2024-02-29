b55b42ce5a00d219902219e8c70cd596161bee52
Tatyana1204 <Smetanina2002sme@gmail.com>
2024-02-29 15:20:37 +0300

    final prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,6 +4,15 @@ import askQuestion from '../helpers/helpers.js';
 
 const TASK = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
+function isPrime(num) {
+
+    for (let i = 2; i < num; i++) {
+      if (num % i === 0) return false;
+    }
+    return num !== 1;
+
+  }
+
 const createRandomNumber = () => {
 
     const number = Math.floor(Math.random() * 101 + 1);
@@ -14,15 +23,6 @@ const createRandomNumber = () => {
         answer
     }
 };
-
-function isPrime(num) {
-
-    for (let i = 2; i < num; i++) {
-      if (num % i === 0) return false;
-    }
-    return num !== 1;
-
-  }
 
 const createPrimeTask = () => {
 
